165daf3347ed22cf80c2a3f50dfcbec9853ee978
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-18 17:38:27 -0300

    feat: add dummy form validation for pratice purposes

--- add.php
+++ add.php
@@ -1,6 +1,54 @@
 <?php
-  if(isset($_GET)){
-    echo $_GET['title'];
+  function checkEmail(string $email){
+    # should not be empty
+    if( empty($email) )
+      echo 'An email is required <br/>';
+
+    # should contain an `@` (at)
+    if( !str_contains($email, "@") ){
+      echo 'Email addresses must have an "@" (at) <br/>';
+      return;
+    }
+
+    # separating user from domain
+    $explodedEmail = explode('@', $email);
+    $emailName = $explodedEmail[0]; # johndoe
+    $emailDomain = $explodedEmail[1]; # gmail.com
+
+    # user should exist
+    if( empty($emailName) )
+      echo "Invalid email user (->email.user.here<-@email.com) <br/>";
+
+    # practice purposes only valid domains list
+    $validEmailDomains = ['gmail.com', 'yahoo.com', 'outlook.com'];
+
+    if( !in_array($emailDomain, $validEmailDomains))
+      echo "Invalid email domain. For practice purposes, we only accept: gmail.com, yahoo.com and outlook.com emails <br/>";
+  }
+
+  function checkTitle(string $title){
+    # should not be empty
+    if( empty($title) ) 
+      echo "Title should not be empty! <br/>";
+  }
+
+  function checkIngredients(array $ingredients){
+    # should not be empty and should not have a length lower than 3
+    if ( empty($ingredients) or count($ingredients) < 3 )
+      echo 'Pizza ingredients list should have at least 3 items (for example: dough, tomato sauce, cheese)';
+  }
+
+  # form validation
+  if(isset($_POST['submit'])){
+    # check email
+    checkEmail($_POST['email']);
+
+    # check title
+    checkTitle($_POST['title']);
+
+    # check ingredients
+    $arrayOfIngredients = explode(',', $_POST['ingredients']);
+    checkIngredients($arrayOfIngredients);
   }
 ?>
 
@@ -23,7 +71,7 @@
       Add a Pizza
     </h2>
 
-    <form action="add.php" method="GET">
+    <form action="add.php" method="POST">
       <!-- user email -->
       <div class="form-group">
         <label for="email">
@@ -43,14 +91,14 @@
       <!-- pizza ingredients -->
       <div class="form-group">
         <label for="ingredients">
-          Pizza ingredients (comma separated)
+          Pizza ingredients (comma separated, minimum of 3)
         </label>
-        <input type="text" name="ingredients" placeholder="Cheese, Tomato, Pepperoni...">
+        <input type="text" name="ingredients" placeholder="Dough, Tomato Sauce, Cheese, Pepperoni...">
       </div>
 
       <!-- submit -->
       <div class="form-group">
-        <input type="submit" value="submit">
+        <input type="submit" name="submit" value="Submit">
       </div>
     </form>
   </div>
