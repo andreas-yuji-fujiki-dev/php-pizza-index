7482bad7b14b7ef64af86679edfe09041be7be58
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-18 15:23:45 -0300

    feat: header links to / and /add.php

--- partials/header.php
+++ partials/header.php
@@ -1,8 +1,12 @@
 <header id="app-header">
-  <h1>
-    Pizza Index
-  </h1>
-  <button>
-    Add a Pizza
-  </button>
+  <a href="/">
+    <h1>
+      Pizza Index
+    </h1>
+  </a>
+  <a href="add.php">
+    <button>
+      Add a Pizza
+    </button>
+  </a>
 </header>
