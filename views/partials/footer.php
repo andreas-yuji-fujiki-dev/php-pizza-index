928a2392189130b0d1bf6e126730475e79857318
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-20 17:06:26 -0300

    refactor: move partials to /views/partials

---
+++ views/partials/footer.php
@@ -0,0 +1,5 @@
+<footer id="app-footer">
+  <span>
+    &copy; 2026 Pizza Index - All rights reserved.
+  </span>
+</footer>
---
+++ views/partials/header.php
@@ -0,0 +1,12 @@
+<header id="app-header">
+  <a href="/">
+    <h1>
+      Pizza Index
+    </h1>
+  </a>
+  <a href="/add.php">
+    <button>
+      Add a Pizza
+    </button>
+  </a>
+</header>
