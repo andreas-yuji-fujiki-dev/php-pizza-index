63256b07f85e9be3d7411330af8997ab3a9d0808
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-20 17:07:00 -0300

    fix: require path

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-  require 'env_variables.php';
+  require __DIR__ . '/../env_variables.php';
   
   $dbConnection = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
   if( !$dbConnection ) echo "Connection error: " . mysqli_connect_error();
