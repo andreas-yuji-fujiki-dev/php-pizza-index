e03b0f8d68b4c3f39e35fc1364a7757765d291c1
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-21 03:05:35 -0300

    feat: details page controller logics and styles

--- views/details.php
+++ views/details.php
@@ -9,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   
-  <link rel="stylesheet" href="/assets/css/styles.css?v82">
+  <link rel="stylesheet" href="/assets/css/styles.css?v521">
 </head>
 <body>
   <?php require 'partials/header.php' ?>
@@ -37,6 +37,93 @@
 
       if( !empty($specificPizza) ): 
     ?>
+      <!-- DELETE confirmation modal -->
+      <?php if($showDeleteModal): ?>
+        <div class="delete-confirm-backdrop"></div>
+        <div class="delete-confirm-modal">
+          <span class="confirmation">
+            Are you sure? This cannot be undone!
+          </span>
+          <span class="pizza-to-delete">
+            The pizza: <?php echo $specificPizza->title ?> will be deleted permanently
+          </span>
+          <form action="/details.php/?id=<?php echo $specificPizza->id?>" method="POST">
+            <input type="hidden" name="id-to-delete" value="<?php echo $specificPizza->id ?>">
+            <input type="submit" name="confirm-delete" value="DELETE">
+            <input type="submit" name="cancel-delete" value="CANCEL">
+          </form>
+
+          <!-- error case -->
+          <?php if($deletionError): ?>
+            <span class="deletion-error">
+              <?php echo $deletionError ?>
+            </span>
+          <?php endif ?>
+        </div>
+      <?php endif ?>
+
+      <!-- EDIT modal -->
+      <?php if($showEditModal): ?>
+        <div class="edit-modal-backdrop"></div>
+        <div class="edit-modal">
+          <!-- title -->
+          <span class="pizza-being-edited">
+            Editing: <?php echo $specificPizza->title ?>
+          </span>
+
+          <!-- form -->
+          <form action="/details.php/?id=<?php echo $specificPizza->id?>" method="POST">
+            <!-- new email -->
+            <div class="form-group">
+              <label for="new-email">
+                New email:
+              </label>
+              <input type="text" name="new-email" placeholder="yournew@email.com" value="<?php echo $editInput_newEmail ?>">
+              <span class="input-error">
+                <?php echo $editInputErrors['email'] ?>
+              </span>
+            </div>
+
+            <!-- new title -->
+            <div class="form-group">
+              <label for="new-title">
+                New title:
+              </label>
+              <input type="text" name="new-title" placeholder="Pepperoni Pizza" value="<?php echo $editInput_newTitle ?>">
+              <span class="input-error">
+                <?php echo $editInputErrors['title'] ?>
+              </span>
+            </div>
+
+            <!-- new ingredients -->
+            <div class="form-group">
+              <label for="new-ingredients">
+                New ingredients:
+              </label>
+              <input type="text" name="new-ingredients" placeholder="Dough, Tomato sauce, Cheese, Pepperoni" value="<?php echo $editInput_newIngredients ?>">
+              <span class="input-error">
+                <?php echo $editInputErrors['ingredients'] ?>
+              </span>
+            </div>
+
+            <!-- action btns  -->
+            <div class="action-buttons">
+              <input type="hidden" name="id-to-update" value="<?php echo $specificPizza->id ?>">
+              <input type="submit" name="cancel-edit" value="CANCEL">
+              <input type="submit" name="confirm-edit" value="CONFIRM">
+            </div>
+          </form>
+
+          <!-- error case -->
+          <?php if($editingError): ?>
+            <span class="edit-error">
+              <?php echo $editingError ?>
+            </span>
+          <?php endif ?>
+        </div>
+      <?php endif ?>
+        
+      <!-- about pizza -->
       <div class="about">
         <div class="details">
           <span class="created-by">
@@ -64,14 +151,12 @@
           </ul>
         </div>
 
+        <!-- action buttons -->
         <div class="action-buttons">
-          <button class="delete">
-            Delete
-          </button>
-
-          <button class="edit">
-            Edit
-          </button>
+          <form action="/details.php/?id=<?php echo $specificPizza->id?>" method="POST">
+            <input type="submit" name="open-delete-modal" class="delete" value="Delete" />
+            <input type="submit" name="open-edit-modal" class="edit" value="Edit" />
+          </form>
         </div>
       </div>
     </div>
