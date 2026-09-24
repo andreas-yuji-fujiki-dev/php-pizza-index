7c1f3b346bbda85c2a96ec8e728da74a90cd90c1
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2026-09-23 21:20:25 -0300

    fix/styles: pizzas spacing

--- views/index.php
+++ views/index.php
@@ -13,7 +13,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
-  <link rel="stylesheet" href="/assets/css/styles.css?v999">
+  <link rel="stylesheet" href="/assets/css/styles.css?v99">
 </head>
 <body>
   <?php require __DIR__ . '/partials/header.php' ?>
